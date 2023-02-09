5a9a6a2fbd9ddbf2b3e7de996a415dea7eb7b17d
TelmoCode <telmogferreira@gmail.com>
2023-02-09 16:57:26 -0300

    media aritimetica

--- dobro_triplo_raisquadrada.py
+++ dobro_triplo_raisquadrada.py
@@ -2,6 +2,6 @@ import math
 
 n = int(input('Digite um numero'))
 
-print('o dobro de {} vale {}'.format(n, (n*2)))
-print('o triplo de {} vale {}'.format(n, (n*3)))
-print('a raiz quadrada de {} vale{} '.format(n, (n**(1/2))))
+print('o dobro de {} vale: {}'.format(n, (n*2)))
+print('o triplo de {} vale: {}'.format(n, (n*3)))
+print('a raiz quadrada de {} vale: {} '.format(n, (n**(1/2))))
